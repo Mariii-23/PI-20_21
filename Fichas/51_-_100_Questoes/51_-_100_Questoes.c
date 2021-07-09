40ddb1b4ca50656784073c4a686592e9a8b75905
Mariii-23 <marianadinis8@hotmail.com>
2021-07-09 12:18:42 +0100

    Add question 50++, 8 to 21

--- Fichas/51_-_100_Questoes/51_-_100_Questoes.c
+++ Fichas/51_-_100_Questoes/51_-_100_Questoes.c
@@ -113,25 +113,211 @@ void splitQS(LInt l, int x, LInt *mx, LInt *Mx) {
 }
 
 /* -------------- 9 ---------------- */
-/* encontra - se incorreto */
 LInt parteAmeio(LInt *l) {
-  LInt *x = l, *y = l;
   LInt lista = *l;
   int i;
   for (i = 0; lista != NULL; lista = lista->prox, i++)
     ;
+
   int n = i / 2;
-
-  for (i = 0; i < n; i++) {
-    *x = (*x)->prox;
-  }
-  // l = x;
-
-  if (n == 0) {
-    l = y;
+  if (n == 0)
     return NULL;
-  } else
-    return *y;
+
+  LInt y = (*l);
+  LInt prev = NULL;
+
+  while (n > 0) {
+    prev = (*l);
+    (*l) = (*l)->prox;
+    n--;
+  }
+  prev->prox = NULL;
+  return y;
+}
+
+/* -------------- 10 ---------------- */
+int removeAll(LInt *l, int elem) {
+  int count = 0;
+
+  while (*l) {
+    if ((*l)->valor == elem) {
+      count++;
+      {
+        // libertar espaco
+        (*l) = (*l)->prox;
+      }
+    } else
+      l = &((*l)->prox);
+  }
+  return count;
+}
+
+/* -------------- 11 ---------------- */
+int removeDups(LInt *l) {
+  int count = 0;
+  while (*l) {
+    count += removeAll(&((*l)->prox), (*l)->valor);
+  }
+  return count;
+}
+
+/* -------------- 12 ---------------- */
+int removeMaiorL(LInt *l) {
+  int max;
+  LInt *maiorL;
+
+  if (*l) {
+    max = (*l)->valor;
+    maiorL = l;
+    l = &((*l)->prox);
+  }
+
+  while (*l) {
+    if ((*l)->valor > max) {
+      max = (*l)->valor;
+      maiorL = l;
+    }
+    l = &((*l)->prox);
+  }
+  {
+    LInt aux = *maiorL;
+    *maiorL = (*maiorL)->prox;
+    free(aux);
+  }
+  return max;
+}
+
+/* -------------- 13 ---------------- */
+void init(LInt *l) {
+  while ((*l) && (*l)->prox) {
+    l = &((*l)->prox);
+  }
+
+  {
+    LInt aux;
+    if ((*l)->prox) {
+      aux = (*l)->prox;
+      (*l)->prox = NULL;
+    } else {
+      aux = (*l);
+      *l = NULL;
+    }
+    free(aux);
+  }
+}
+
+/* -------------- 14 ---------------- */
+void appendL(LInt *l, int value) {
+  LInt novo = malloc(sizeof(struct lligada));
+  novo->prox = NULL;
+  novo->valor = value;
+
+  while ((*l) && (*l)->prox) {
+    l = &((*l)->prox);
+  }
+
+  if (!(*l))
+    *l = novo;
+  else
+    (*l)->prox = novo;
+}
+
+/* -------------- 15 ---------------- */
+void concatL(LInt *a, LInt b) {
+  while ((*a) && (*a)->prox) {
+    a = &((*a)->prox);
+  }
+
+  if (!(*a))
+    *a = b;
+  else
+    (*a)->prox = b;
+}
+
+/* -------------- 16 ---------------- */
+LInt cloneL(LInt l) {
+  LInt clone, *e = &clone;
+
+  while (l) {
+    (*e) = malloc(sizeof(struct lligada));
+    (*e)->valor = l->valor;
+    e = &((*e)->prox);
+    l = l->prox;
+  }
+  *e = NULL;
+  return clone;
+}
+
+/* -------------- 17 ---------------- */
+LInt cloneRev(LInt l) {
+  LInt clone = NULL;
+  while (l) {
+    {
+      LInt e = malloc(sizeof(struct lligada));
+      e->valor = l->valor;
+      e->prox = clone;
+      clone = e;
+    }
+    l = l->prox;
+  }
+  return clone;
+}
+
+/* -------------- 18 ---------------- */
+int maximo(LInt l) {
+  if (!l)
+    return -1;
+  int max = l->valor;
+  l = l->prox;
+  while (l) {
+    if (max < l->valor)
+      max = l->valor;
+    l = l->prox;
+  }
+  return max;
+}
+
+/* -------------- 19 ---------------- */
+int take(int n, LInt *l) {
+  int i = 0;
+  while (n > 0 && (*l)) {
+    i++;
+    n--;
+    l = &((*l)->prox);
+  }
+
+  while (*l) {
+    {
+      LInt aux = *l;
+      *l = (*l)->prox;
+      aux->prox = NULL;
+      free(aux);
+    }
+  }
+  return i;
+}
+
+/* -------------- 20 --------------- */
+int drop(int n, LInt *l) {
+  int i = 0;
+  while (n > 0 && (*l)) {
+    i++;
+    n--;
+    {
+      LInt aux = *l;
+      *l = (*l)->prox;
+      aux->prox = NULL;
+      free(aux);
+    }
+  }
+  return i;
+}
+
+/* -------------- 21 --------------- */
+LInt Nforward(LInt l, int N) {
+  while (N-- > 0)
+    l = l->prox;
+  return l;
 }
 
 /* -------------- MAIN --------------- */
